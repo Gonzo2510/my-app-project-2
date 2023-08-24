f189b7eb630b1f7c2a74bdc0654953de524494f3
Jacob Gonzales <jagonz@live.com>
2023-08-24 18:31:09 -0500

    moved db inside src folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React, { useState } from "react";
 import {Routes, Route} from "react-router-dom";
 import NavBar from "./NavBar";
-import Contacts from "./Contacts";
+//import Contacts from "./Contacts";
 import Home from "./Home";
 import AddContact from "./AddContact";
 import "./index.css"
+
+function Contact({ contact }) {
+  const { name, address, phoneNumber} = contact
+
+  console.log(name, address, phoneNumber)
+
+  return (
+
+      <div>
+          {name}
+          {address}
+          {phoneNumber}
+      </div>
+  )
+}
+
+
+function Contacts({ contacts, setContacts}) {
+  let [contactList, setContactList] = useState([])
+  
+      fetch('http://localhost:3000/contacts')
+      .then(response => response.json())
+      .then(contacts => console.log(contacts))
+      .then(contacts => setContacts= (contacts.forEach(contact => <Contact contact={contact} />)))
+      //.then(console.log(contactList))
+
+  return (
+      <>
+      {contacts}
+      </>
+  )
+}
+
 
 function App() {
   const [contacts, setContacts] = useState([])
