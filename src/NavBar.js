bd726cfd09fc74904fe4c67c5c2fc289b98d68d0
Gonzo2510 <Jagonz@live.com>
2023-09-12 16:51:43 -0500

    centered navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
   return (
     <div className="navbar">
-      <h2>Contacts Book</h2>
+      <h1>Contacts Book</h1>
         <NavLink exact='/home' to ='/home' >Home</NavLink>
         <NavLink exact='/contactList' to ='/contactList' >Contacts</NavLink>
         <NavLink exact='/addContact' to ='/addContact' >Add Contact</NavLink>
